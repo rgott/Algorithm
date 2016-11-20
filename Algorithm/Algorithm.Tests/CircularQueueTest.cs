558e21e7a3bfb597c70d50fcccb9627ddd1d1446
Robert Gott <robgott@yahoo.com>
2016-11-20 12:37:02 -0500

    Circular overflow queue with tests

---
+++ Algorithm/Algorithm.Tests/CircularQueueTest.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Algorithm.Tests
+{
+    [TestClass]
+    public class CircularQueueTest
+    {
+        public CircularOverflowQueue<int> COFQ { get; set; }
+
+        [TestMethod]
+        public void BasicOperations1()
+        {
+            COFQ = new CircularOverflowQueue<int>(4);
+            COFQ.Enqueue(0);
+            COFQ.Enqueue(1);
+            COFQ.Enqueue(2);
+            COFQ.Enqueue(3);
+
+            Assert.AreEqual(0, COFQ.Dequeue());
+            Assert.AreEqual(1, COFQ.Dequeue());
+            Assert.AreEqual(2, COFQ.Dequeue());
+            Assert.AreEqual(3, COFQ.Dequeue());
+        }
+
+        [TestMethod]
+        public void BasicOperations2()
+        {
+            COFQ = new CircularOverflowQueue<int>(4);
+            COFQ.Enqueue(0);
+            COFQ.Enqueue(1);
+            COFQ.Enqueue(2);
+            COFQ.Enqueue(3);
+
+            Assert.AreEqual(0, COFQ.Dequeue());
+            Assert.AreEqual(1, COFQ.Dequeue());
+
+            COFQ.Enqueue(4);
+            COFQ.Enqueue(5);
+
+            Assert.AreEqual(2, COFQ.Dequeue());
+            Assert.AreEqual(3, COFQ.Dequeue());
+            Assert.AreEqual(4, COFQ.Dequeue());
+            Assert.AreEqual(5, COFQ.Dequeue());
+
+        }
+
+        [TestMethod]
+        public void underflow()
+        {
+            COFQ = new CircularOverflowQueue<int>(1);
+            try
+            {
+                COFQ.Dequeue();// should fail
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                // passed test
+            }
+        }
+
+        [TestMethod]
+        public void overflow()
+        {
+            COFQ = new CircularOverflowQueue<int>(2);
+            COFQ.Enqueue(1);
+            COFQ.Enqueue(2);
+            COFQ.Enqueue(3);// should wrap around (pass)
+        }
+
+        [TestMethod]
+        public void mutlipleOverflow()
+        {
+            COFQ = new CircularOverflowQueue<int>(2);
+            COFQ.Enqueue(1);
+            COFQ.Enqueue(2);
+            COFQ.Enqueue(3);// All passed this should wrap around (pass)
+            COFQ.Enqueue(4);
+            COFQ.Enqueue(5);
+            COFQ.Enqueue(6);
+            COFQ.Enqueue(7);
+            COFQ.Enqueue(8);
+            COFQ.Enqueue(9);
+            
+            Assert.AreEqual(9, COFQ.Dequeue());
+            Assert.AreEqual(8, COFQ.Dequeue());
+        }
+    }
+}
---
+++ Algorithm/Algorithm/CircularQueue.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Algorithm
+{
+    /// <summary>
+    /// OverWrittable circular queue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CircularOverflowQueue<T>
+    {
+        private T[] Items;
+        private int enqueuePos;
+        private int dequeuePos;
+
+        /// <summary>
+        /// Maximum number of items that can fit in the queue.
+        /// </summary>
+        public int MaxSize { get; private set; }
+
+        /// <summary>
+        /// Current Number of items in the queue.
+        /// </summary>
+        public int Size { get; private set; }
+
+
+        /// <summary>
+        /// Set array behind the scenes
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Value at index.</returns>
+        public T this[int index]
+        {
+            get
+            {
+                return Items[index];
+            }
+            set
+            {
+                Items[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MaxSize">Number of elements can be added to queue. Anything values enqueued over will be overwritten</param>
+        public CircularOverflowQueue(int MaxSize)
+        {
+            Items = new T[MaxSize];
+            this.MaxSize = MaxSize;
+            Size = 0;
+
+            // start before beginning 
+            // move to position then set value
+            enqueuePos = -1; 
+            dequeuePos = -1;
+        }
+
+        /// <summary>
+        /// Add items to queue. Items will be overwritten if too many are added.
+        /// </summary>
+        /// <param name="item">Add to queue</param>
+        public void Enqueue(T item)
+        {
+            // if out of bounds reset
+            if (enqueuePos + 1 >= MaxSize)
+                enqueuePos = 0;
+            else
+                enqueuePos++;
+
+            if (Size < MaxSize)// bound size
+                Size++;
+            else // if maxSize then enqueue will pass dequeue. Keep dequeue ahead
+                dequeuePos++;
+
+            Items[enqueuePos] = item; // set item
+        }
+
+        /// <summary>
+        /// Remove items from queue.
+        /// </summary>
+        /// <exception cref="IndexOutOfRangeException">If no elements are in queue.</exception>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+            if (dequeuePos + 1 >= MaxSize) // if out of bounds reset
+                dequeuePos = -1;
+
+            if (Size-- <= 0)
+                throw new IndexOutOfRangeException("No elements in queue.");
+
+            return Items[++dequeuePos];
+        }
+    }
+}
