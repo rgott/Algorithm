f41e8205359e3fa4e6997ff942b7a03bc59919f0
Robert Gott <robgott@yahoo.com>
2016-11-20 18:33:56 -0500

    Bubble, Selection, Insertion with tests

---
+++ Algorithm/Algorithm.Tests/SortTest.cs
@@ -0,0 +1,55 @@
+﻿using Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Tests
+{
+    [TestClass]
+    public class SortTest
+    {
+        string[] Items;
+        string[] sortedList;
+        public SortTest()
+        {
+            Items = new string[] { "C", "A", "B" , "Z","M","L","V","F","E","D"};
+            sortedList = new string[] { "A","B","C","D","E","F","L","M","V","Z" };
+        }
+
+        [TestMethod]
+        public void Bubble()
+        {
+            string[] item = new string[Items.Length];
+            Array.Copy(Items, item,Items.Length);
+
+            Sort<string>.Bubble(ref item);
+
+            CollectionAssert.AreEqual(sortedList,item);
+        }
+
+        [TestMethod]
+        public void Selection()
+        {
+            string[] item = new string[Items.Length];
+            Array.Copy(Items, item, Items.Length);
+
+            Sort<string>.Selection(ref item);
+
+            CollectionAssert.AreEqual(sortedList, item);
+        }
+
+        [TestMethod]
+        public void Insertion()
+        {
+            string[] item = new string[Items.Length];
+            Array.Copy(Items, item, Items.Length);
+
+            Sort<string>.Insertion(ref item);
+
+            CollectionAssert.AreEqual(sortedList, item);
+        }
+    }
+}
---
+++ Algorithm/Algorithm/Sort.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class Sort<T> where T : IComparable
+    {
+        private static void Swap(ref T[] array, int swapIndex1, int swapIndex2)
+        {
+            T temp = array[swapIndex1];
+            array[swapIndex1] = array[swapIndex2];
+            array[swapIndex2] = temp;
+        }
+
+
+        public static void Bubble(ref T[] array)
+        {
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true; // always assume sorted until found not to be
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if ((array[i] as IComparable).CompareTo(array[i + 1]) == 1) // (first > second)? then swap
+                    {
+                        isSorted = false;
+
+                        Swap(ref array, i, i + 1);
+                    }
+                }
+            }
+            // now sorted
+        }
+
+        public static void Selection(ref T[] array)
+        {
+            if(array.Length > 1)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int minValue = i;
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        if((array[minValue] as IComparable).CompareTo(array[j]) == 1) // first < second then second = max
+                            minValue = j;
+                    }
+
+                    if(minValue != i)
+                        Swap(ref array, minValue, i);
+                }
+            }
+        }
+
+        public static void Insertion(ref T[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if ((array[i] as IComparable).CompareTo(array[i + 1]) == 1) // first > second then swap and check list
+                {
+                    // swap
+                    Swap(ref array, i, i + 1);
+
+                    // check list
+                    for (int j = i; j > 0; j--)
+                    {
+                        if ((array[j] as IComparable).CompareTo(array[j - 1]) == -1)// if not in order
+                            Swap(ref array, j, j - 1);
+                    }
+                }
+            }
+        }
+    }
+}
