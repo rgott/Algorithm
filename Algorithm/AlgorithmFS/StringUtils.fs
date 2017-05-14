fa1004e87531ba98ae0bd48d3a186096718de5a3
Robert Gott <robgott@yahoo.com>
2017-05-13 21:08:50 -0400

    rename directories
    remove unused project

--- Algorithm/AlgorithmFS/StringUtils.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace AlgorithmFS
-
-open System;
-
-module StringUtils = 
-    let IsPalindrome (word:string) = 
-        let rev (word:string) = 
-            String(Array.rev(word.ToCharArray()))
-        word = rev word
